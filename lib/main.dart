1daaa74798a9110c73ca95b2c6ebfbb904b7135c
SABoev20 <85336777+SABoev20@users.noreply.github.com>
2023-07-03 23:15:23 +0300

    Add home page and date selector

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:intl/intl.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 void main() {
   runApp(NasaDailyApp());
@@ -23,25 +25,73 @@ class NasaDailyApp extends StatelessWidget {
   }
 }
 
-class NasaDailyScreen extends StatefulWidget {
+class NasaDailyScreen extends StatelessWidget {
   @override
-  _NasaDailyScreenState createState() => _NasaDailyScreenState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        toolbarHeight: 60,
+        title: Text(
+          'NASA Daily',
+          style: TextStyle(
+            fontSize: 26,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.black,
+              Colors.black,
+              Colors.deepPurple,
+            ],
+          ),
+        ),
+        child: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => HomePage()),
+              );
+            },
+            child: Text('Go to Home Page'),
+          ),
+        ),
+      ),
+    );
+  }
 }
 
-class _NasaDailyScreenState extends State<NasaDailyScreen> {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   String _title = '';
   String _date = '';
   String _description = '';
   String _imageUrl = '';
 
-  @override
-  void initState() {
-    super.initState();
-    fetchNasaDailyPhoto();
-  }
+  DateTime _selectedDay = DateTime.now();
+  DateTime _focusedDay = DateTime.now();
+  CalendarFormat _calendarFormat = CalendarFormat.month;
 
-  Future<void> fetchNasaDailyPhoto() async {
-    final response = await http.get(Uri.parse('https://api.nasa.gov/planetary/apod?api_key=IORgClb8l9mBWULEl0XVw1HTPrMWaV98v4OqFl53'));
+  Map<DateTime, List<dynamic>> _events = {};
+
+  Future<void> fetchNasaDailyPhoto(DateTime date) async {
+    final formattedDate = DateFormat('yyyy-MM-dd').format(date);
+    final response = await http.get(
+      Uri.parse('https://api.nasa.gov/planetary/apod?api_key=IORgClb8l9mBWULEl0XVw1HTPrMWaV98v4OqFl53&date=$formattedDate'),
+    );
 
     if (response.statusCode == 200) {
       final jsonData = jsonDecode(response.body);
@@ -56,109 +106,160 @@ class _NasaDailyScreenState extends State<NasaDailyScreen> {
     }
   }
 
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    setState(() {
+      _selectedDay = selectedDay;
+      _focusedDay = focusedDay;
+    });
+    fetchNasaDailyPhoto(selectedDay);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
         backgroundColor: Colors.black,
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          toolbarHeight: 60,
-          title: Text(
-              ' NASA Daily',
-              style: TextStyle(
-                fontSize: 26,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              )
-          )
-        ),
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Colors.black,
-                Colors.black,
-                Colors.deepPurple,
-              ],
-            ),
+        toolbarHeight: 60,
+        title: Text(
+          'NASA Daily',
+          style: TextStyle(
+            fontSize: 26,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
           ),
-          child: Theme(
-            data: Theme.of(context).copyWith(
-              highlightColor: Colors.purple,
-              splashColor: Colors.purple,
-              colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.black26),
-            ),
-            child: Scrollbar(
-              child: GlowingOverscrollIndicator(
-                axisDirection: AxisDirection.down,
-                color: Colors.black,
-                showLeading: false,
-                showTrailing: false,
-                child: SingleChildScrollView(
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.black,
+              Colors.black,
+              Colors.deepPurple,
+            ],
+          ),
+        ),
+        child: Theme(
+          data: Theme.of(context).copyWith(
+            highlightColor: Colors.purple,
+            splashColor: Colors.purple,
+            colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.black26),
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                if (_imageUrl.isNotEmpty)
+                  Image.network(
+                    _imageUrl,
+                    fit: BoxFit.cover,
+                    width: 380,
+                  ),
+                Padding(
+                  padding: const EdgeInsets.all(38.0),
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      if (_imageUrl.isNotEmpty)
-                        Image.network(
-                          _imageUrl,
-                          fit: BoxFit.cover,
-                          width: 380,
-                        ),
-                      Padding(
-                        padding: const EdgeInsets.all(38.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              _title,
-                              style: TextStyle(
-                                fontSize: 26,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            SizedBox(height: 8),
-                            Text(
-                              _date,
-                              style: TextStyle(
-                                fontSize: 16,
-                                color: Colors.white38,
-                              ),
-                            ),
-                            SizedBox(height: 18),
-                            Text(
-                              _description,
-                              style: TextStyle(
-                                fontSize: 19,
-                                color: Colors.white70,
-                              ),
-                            ),
-                          ],
+                      Text(
+                        _title,
+                        style: TextStyle(
+                          fontSize: 26,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                      Container(
-                        color: Colors.black,
-                        width: 420,
-                        height: 50,
-                        padding: EdgeInsets.all(16),
-                        child: Text(
-                          'Credits: NASA',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                          ),
+                      SizedBox(height: 8),
+                      Text(
+                        _date,
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white38,
+                        ),
+                      ),
+                      SizedBox(height: 18),
+                      Text(
+                        _description,
+                        style: TextStyle(
+                          fontSize: 19,
+                          color: Colors.white70,
                         ),
                       ),
                     ],
                   ),
                 ),
-              ),
+                Container(
+                  color: Colors.black,
+                  width: 420,
+                  height: 50,
+                  padding: EdgeInsets.all(16),
+                  child: Text(
+                    'Credits: NASA',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                  child: Column(
+                    children: [
+                      TableCalendar(
+                        firstDay: DateTime.utc(2010, 1, 1),
+                        lastDay: DateTime.utc(2030, 12, 31),
+                        focusedDay: _focusedDay,
+                        selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+                        calendarFormat: _calendarFormat,
+                        onDaySelected: _onDaySelected,
+                        onPageChanged: (focusedDay) {
+                          _focusedDay = focusedDay;
+                        },
+                        calendarStyle: CalendarStyle(
+                          todayDecoration: BoxDecoration(
+                            color: Colors.purple,
+                            shape: BoxShape.circle,
+                          ),
+                          selectedDecoration: BoxDecoration(
+                            color: Colors.purpleAccent,
+                            shape: BoxShape.circle,
+                          ),
+                          selectedTextStyle: TextStyle(color: Colors.white),
+                          weekendTextStyle: TextStyle(color: Colors.white),
+                          holidayTextStyle: TextStyle(color: Colors.white),
+                          outsideTextStyle: TextStyle(color: Colors.grey),
+                        ),
+                        headerStyle: HeaderStyle(
+                          formatButtonVisible: false,
+                          titleCentered: true,
+                          titleTextStyle: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          leftChevronIcon: Icon(
+                            Icons.chevron_left,
+                            color: Colors.white,
+                          ),
+                          rightChevronIcon: Icon(
+                            Icons.chevron_right,
+                            color: Colors.white,
+                          ),
+                          headerPadding: EdgeInsets.symmetric(vertical: 16),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
-        )
+        ),
+      ),
     );
   }
 }
